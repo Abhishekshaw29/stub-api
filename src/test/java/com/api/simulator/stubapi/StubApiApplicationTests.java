e41c8e4371115f781660985f0324342281c092e9
Abhishek shaw <abhishekshaw813@gmail.com>
2024-04-04 19:00:25 +0530

    first commit

---
+++ src/test/java/com/api/simulator/stubapi/StubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.simulator.stubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
